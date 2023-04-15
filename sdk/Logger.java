5fcea2bbc4cc2a94decd3c49e27981ebb57f37b0
Degtiarev_Viktor <umkaquark@gmail.com>
2023-04-15 21:38:00 +0300

    log

--- sdk/Logger.java
+++ sdk/Logger.java
@@ -1,4 +1,16 @@
 package sdk;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Logger {
+
+    List<String[]> logs = new LinkedList<>();
+    public Logger() {
+    }
+
+    public void log(String IdDevice, String TimeMili, String Location, String ModelDevice) {
+
+    }
+
 }
