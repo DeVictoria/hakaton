623a7dafa746f91296d23d15a9a83bfc672480e1
Degtiarev_Viktor <umkaquark@gmail.com>
2023-04-15 23:07:03 +0300

    лог2

--- sdk/Logger.java
+++ sdk/Logger.java
@@ -6,11 +6,20 @@ import java.util.List;
 public class Logger {
 
     List<String[]> logs = new LinkedList<>();
+
     public Logger() {
     }
 
     public void log(String IdDevice, String TimeMili, String Location, String ModelDevice) {
-
+        String[] log = new String[4];
+        log[0] = IdDevice;
+        log[1] = TimeMili;
+        log[2] = Location;
+        log[3] = ModelDevice;
+        logs.add(log);
+        if(logs.size()>=100){
+            SenderToServer();
+        }
     }
 
 }
